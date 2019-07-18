7a2d228bb13fc3e200488f8b6cb5c9845654ac8c
Alexander Sokol <sokol@dualitygroup.com>
2019-07-18 14:08:40 -0400

    Added Apache-2.0 license file and the package stub.

---
+++ py/src/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datacentric",
+    version="2.1.0",
+    author="The DataCentric Authors",
+    author_email="support@datacentric.org",
+    description="Core services library for data-centric development.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/datacentricorg/datacentric-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
