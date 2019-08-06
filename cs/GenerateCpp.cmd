e8182b70b9b0e38dd37e824e0cd54b77f6fd8d17
CompatibL <team@compatibl.com>
2019-08-06 13:47:42 -0400

    Added script to generate C++ stubs from compiled C# DLL.
    (cherry picked from commit b3a6b13dc038c90e2299ca623f848289dafa2bb2)

---
+++ cs/GenerateCpp.cmd
@@ -0,0 +1,4 @@
+dotnet src\DataCentric.Cli\bin\Debug\netcoreapp2.1\DataCentric.Cli.Cs.dll headers ^
+--assembly  src\DataCentric\bin\Debug\netstandard2.0\DataCentric.dll ^
+--output ..\cpp\src\datacentric ^
+--project src\
