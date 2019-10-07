506d7a3e997a8f228ec28dac4cd6043c4c44a97e
Alexander Sokol <sokol@dualitygroup.com>
2019-10-06 22:37:47 -0400

    Updated approved files after log changes.

--- cs/src/DataCentric.Test/Platform/Approvals/FixtureTest.cs
+++ cs/src/DataCentric.Test/Platform/Approvals/FixtureTest.cs
@@ -29,13 +29,19 @@ namespace DataCentric.Test
         {
             using (var context = new UnitTestContext(this))
             {
-                // Log entries other than Exception
+                // Warning and Info entries
                 context.Log.Warning("Warning log entry.");
                 context.Log.Info("Status log entry.");
 
                 // Verify entries
-                context.Log.Verify("Single-line verify entry.");
-                context.Log.Verify("Title", "Multi-line verify entry.");
+                context.Log.Verify("Single-line verify message.");
+                context.Log.Verify("Multi-line verify message." + Environment.NewLine + "Second line of message.");
+                context.Log.Verify("Single-line verify title", "Single-line verify body.");
+                context.Log.Verify(
+                    "Multi-line verify title" + Environment.NewLine + "Second line of title.",
+                    "Multi-line verify body." + Environment.NewLine + "Second line of body.");
+
+                // Asserts
                 context.Log.Assert(true, "Assert when condition is true.");
                 context.Log.Assert(false, "Assert when condition is false.");
             }
