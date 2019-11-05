db539822d971832e7b8a5c9d8a89cc1f2c0f3873
Alexander Sokol <sokol@dualitygroup.com>
2019-11-05 18:47:55 -0500

    Added pymongo to the list of required packages.
    (cherry picked from commit 5beeefc10bcc0be0414079826a4ee720cf0e597c)

--- py/src/setup.py
+++ py/src/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Core services library for data-centric development.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['typing_inspect>=0.4', 'numpy>=1.17'],
+    install_requires=['typing_inspect>=0.4', 'numpy>=1.17', 'pymongo>=3.9.0'],
     url="https://github.com/datacentricorg/datacentric-py",
     packages=setuptools.find_packages(include=('datacentric', 'datacentric.*'), exclude=['test', 'test.*']),
     classifiers=[
